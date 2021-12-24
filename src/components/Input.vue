cd55ae0113084b5ae64fe11ab4a180fb605bfa57
Testuser <toryali.omary@stude>
2021-12-24 14:49:23 +0100

    adds entity

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,33 +1,63 @@
 <template>
-  <div class="mb-3 row">
-    <label class="col-sm-2 col-form-label">Webseite</label>
-    <div class="col-sm-10">
-      <input type="text"  class="form-control-plaintext" value="www.Example.de">
+  <form class="row g-3">
+    <p> </p>
+    <div class="center" >
+      <label for="website" class="form-label">Webseite</label>
+      <input type="website" class="form-control" id="website" v-model="webseite">
     </div>
-  </div>
-  <div class="mb-3 row">
-    <label class="col-sm-2 col-form-label">Passwort</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword">
+    <p> </p>
+    <div class="col-md-6">
+      <label for="inputPassword4" class="form-label">Passwort</label>
+      <input type="password" class="form-control" id="password" v-model="passwort">
     </div>
-  </div>
-  <div class="mb-3 row">
-    <label class="col-sm-2 col-form-label">Passwort erneut eingeben</label>
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword">
+    <div class="col-md-6">
+      <label for="passwortCheck" class="form-label">Passwort-Check</label>
+      <input type="password" class="form-control" id="passwordCheck" v-model="passwortCheck">
     </div>
-  </div>
-  <div class="mb-3 row">
-    <label class="col-sm-2 col-form-label">Arbeitsbereich eingeben</label>
-    <div class="col-sm-10">
-      <input type="text" class="form-control">
+    <div class="col-12">
+      <button type="submit" class="btn btn-primary" @click.prevent="createPassword">Submit</button>
     </div>
-  </div>
+  </form>
 </template>
 
 <script>
+
 export default {
-  name: 'Input'
+  name: 'Input',
+  data () {
+    return {
+      webseite: '',
+      passwort: '',
+      passwortCheck: '',
+      arbeitsbereich: ''
+    }
+  },
+  methods: {
+    createPassword () {
+      console.log(this.webseite)
+      console.log(this.passwort)
+      console.log(this.passwortCheck)
+
+      const endpoint = process.env.VUE_APP_BACKEND_BASE_URL + '/api/passwort'
+      const headers = new Headers()
+      headers.append('Content-Type', 'application/json')
+
+      const playload = JSON.stringify({
+        webseite: this.webseite,
+        passwort: this.passwort
+      })
+
+      const requestOptions = {
+        method: 'POST',
+        headers: headers,
+        body: playload,
+        redirect: 'follow'
+      }
+
+      fetch(endpoint, requestOptions)
+        .catch(error => console.log('error', error))
+    }
+  }
 }
 </script>
 
